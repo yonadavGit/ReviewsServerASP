be0e9542545e81de8148129a809c6e9154673fc1
Yona Dav <yonadav.le@gmail.com>
2022-05-25 18:34:11 +0300

    first commit

---
+++ reviewsForChatApp/WebApplication1/Models/Review.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Range(1, 5, ErrorMessage = "Range of rating is 1-5")]
+        public int Rating { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime dateTime { get; set; }
+    }
+}
